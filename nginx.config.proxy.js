64e05cf4d4be6668c8022659be1a1cc15df5b6a0
cat <1013056502@qq.com>
2020-04-13 14:43:33 +0800

    增加更多配置

--- nginx.config.proxy.js
+++ nginx.config.proxy.js
@@ -1,12 +1,53 @@
-#部署 nginx 配置反向代理 中转cooke和接口
-http{
-    server {
-        listen       9090; 
-	    
-        #websocket代理使用
-        map $http_upgrade $connection_upgrade {default upgrade; '' close;}
 
-        #将系统应用文件放入html 根目录下
+#user  nobody;
+worker_processes  1;
+
+#error_log  logs/error.log;
+#error_log  logs/error.log  notice;
+#error_log  logs/error.log  info;
+
+#pid        logs/nginx.pid;
+
+
+events {
+    worker_connections  1024;
+}
+
+
+http {
+    include       mime.types;
+    default_type  application/octet-stream;
+
+    #log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+    #                  '$status $body_bytes_sent "$http_referer" '
+    #                  '"$http_user_agent" "$http_x_forwarded_for"';
+
+    #access_log  logs/access.log  main;
+
+    sendfile        on;
+    #tcp_nopush     on;
+
+    #keepalive_timeout  0;
+    keepalive_timeout  65;
+
+    #gzip  on;
+    
+	#websocket代理使用
+    map $http_upgrade $connection_upgrade {
+	  default upgrade; 
+	  '' close;
+	}
+    
+
+    server {
+        listen       9090;
+        server_name  localhost;
+
+        #charset koi8-r;
+
+        #access_log  logs/host.access.log  main;
+        
+		#将系统应用文件放入html 根目录下
         location / {
             root   html;
             index  index.html index.htm;	
@@ -18,17 +59,85 @@ http{
 		   proxy_pass http://193.169.100.222:8087;		    
 		   
 		   #cookie 路径映射
-		   proxy_cookie_path /itms /;
+		   proxy_cookie_path /itms /;		    
 		   
-           #websocket代理配置
+           #websocket代理配置		   
+		   proxy_http_version 1.1; 
+           #设置超时时间为24小时  		   
+		   proxy_read_timeout 86400s;		   
 		   proxy_set_header Upgrade $http_upgrade;
-           proxy_set_header Connection "upgrade";
-           proxy_read_timeout 2120000s;
-		   
-		   #proxy_http_version 1.1;
-           #proxy_set_header Upgrade $http_upgrade; 
-           #proxy_set_header Connection $connection_upgrade;
-           #proxy_set_header Origin "";		   
+           #proxy_set_header Connection "upgrade";
+           proxy_set_header Connection $connection_upgrade;           
 		}			
+         
+        #error_page  404              /404.html;
+
+        # redirect server error pages to the static page /50x.html
+        #
+        error_page   500 502 503 504  /50x.html;
+        location = /50x.html {
+            root   html;
+        }
+
+        # proxy the PHP scripts to Apache listening on 127.0.0.1:80
+        #
+        #location ~ \.php$ {
+        #    proxy_pass   http://127.0.0.1;
+        #}
+
+        # pass the PHP scripts to FastCGI server listening on 127.0.0.1:9000
+        #
+        #location ~ \.php$ {
+        #    root           html;
+        #    fastcgi_pass   127.0.0.1:9000;
+        #    fastcgi_index  index.php;
+        #    fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
+        #    include        fastcgi_params;
+        #}
+
+        # deny access to .htaccess files, if Apache's document root
+        # concurs with nginx's one
+        #
+        #location ~ /\.ht {
+        #    deny  all;
+        #}
+		
     }
-} 
+
+
+    # another virtual host using mix of IP-, name-, and port-based configuration
+    #
+    #server {
+    #    listen       8000;
+    #    listen       somename:8080;
+    #    server_name  somename  alias  another.alias;
+
+    #    location / {
+    #        root   html;
+    #        index  index.html index.htm;
+    #    }
+    #}
+
+
+    # HTTPS server
+    #
+    #server {
+    #    listen       443 ssl;
+    #    server_name  localhost;
+
+    #    ssl_certificate      cert.pem;
+    #    ssl_certificate_key  cert.key;
+
+    #    ssl_session_cache    shared:SSL:1m;
+    #    ssl_session_timeout  5m;
+
+    #    ssl_ciphers  HIGH:!aNULL:!MD5;
+    #    ssl_prefer_server_ciphers  on;
+
+    #    location / {
+    #        root   html;
+    #        index  index.html index.htm;
+    #    }
+    #}
+
+}
